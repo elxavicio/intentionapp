299f3c246eae3f52d28fedeb2564b6fd68005d2c
Javier Carceller <elxavicio@gmail.com>
2019-01-16 16:29:10 +0800

    MVP done

--- intention.js
+++ intention.js
@@ -1,1 +1,6 @@
-window.prompt(`Why are you accessing ${window.location.hostname}`,"I am bored");
+if (confirm(`Are you sure you want to spend time on ${window.location.hostname}`)) {
+    alert(`I'm so disappointed`);
+  } else {
+    alert(`I'm proud of you!`);
+    window.location = 'http://www.donothingfor2minutes.com/';
+  } 
